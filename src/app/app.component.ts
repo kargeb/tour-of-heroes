5318b1c65ac04de2b72b9b6ea35ebeba96033a4b
Karol Gebarowski <karol.gebarowski@gmail.com>
2021-02-11 08:42:18 +0100

    add my own functionality to toggle messageBox

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router } from '@angular/router';
 import { slideInAnimation } from './animations';
 
 @Component({
@@ -15,4 +15,23 @@ export class AppComponent {
       outlet && outlet.activatedRouteData && outlet.activatedRouteData.animation
     );
   }
+
+  constructor(private router: Router) {}
+
+  showMessageBox(): void {
+    const url = this.router.url;
+    if (url.includes('popup')) {
+      this.router.navigate([{ outlets: { popup: null } }]);
+    } else {
+      this.router.navigate([{ outlets: { popup: ['compose'] } }]);
+    }
+  }
+
+  // showUrlParams(): boolean {
+  //   console.log();
+  //   console.log('TO SA PARAMETRY URL:', this.router.url);
+  //   const url = this.router.url;
+
+  //   return url.includes('popup')
+  // }
 }
